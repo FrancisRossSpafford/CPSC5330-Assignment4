6514dbefe71b6ec40b3c95c91e6915bff3ae9e28
Francis Ross Spafford <spafffr@auburn.edu>
2025-02-09 15:34:51 -0600

    Initial Commit

---
+++ Assignment4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment4
+//
+//  Created by Ross Spafford on 2/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
